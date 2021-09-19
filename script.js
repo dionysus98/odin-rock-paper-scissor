61cb9e4b769e27c25429f4960ddc91f003edcc20
Avinash S <avinashthebigboss@gmail.com>
2021-09-19 14:59:31 +0530

    ADD index.html style.css script.js

--- script.js
+++ script.js
@@ -5,15 +5,54 @@
 // let answer = Math.trunc(Math.random() * 3) + 1;
 // console.log(answer);
 
+const rock = document.getElementById('player-rock');
+const scissor = document.getElementById('player-scissor');
+const paper = document.getElementById('player-paper');
+const score = document.getElementById('player-score');
+const scoreComp = document.getElementById("computer-score");
+const winner = document.getElementById('winner');
+const imgPlayer = document.getElementById('player-img');
+const imgComp = document.getElementById('computer-img');
+const computerRps = document.getElementById("computer-rps");
+
+
+rock.addEventListener("click", function rock() {
+    imgPlayer.src = "https://www3.bostonglobe.com//rw/Boston/2011-2020/WebGraphics/Metro/BostonGlobe.com/2018/05/rockpaperscissors/assets/rock--left.png";
+    imgComp
+    imgComp.src = `https://www3.bostonglobe.com//rw/Boston/2011-2020/WebGraphics/Metro/BostonGlobe.com/2018/05/rockpaperscissors/assets/${computerplay()}--left.png`;
+
+
+
+    // console.log(this.textContent);
+    // if (this.textContent === computerplay()) return;
+    // else if (this.textContent && computerplay() === "Scissor") {
+    //     score.textContent = score + 1;
+    // }
+
+})
+
+paper.addEventListener("click", function paper() {
+    imgPlayer.src = "https://www3.bostonglobe.com//rw/Boston/2011-2020/WebGraphics/Metro/BostonGlobe.com/2018/05/rockpaperscissors/assets/paper--left.png"
+    imgComp.src = `https://www3.bostonglobe.com//rw/Boston/2011-2020/WebGraphics/Metro/BostonGlobe.com/2018/05/rockpaperscissors/assets/${computerplay()}--left.png`;
+})
+
+scissor.addEventListener("click", function scissor() {
+    imgPlayer.src = "https://www3.bostonglobe.com//rw/Boston/2011-2020/WebGraphics/Metro/BostonGlobe.com/2018/05/rockpaperscissors/assets/scissors--left.png"
+    imgComp.src = `https://www3.bostonglobe.com//rw/Boston/2011-2020/WebGraphics/Metro/BostonGlobe.com/2018/05/rockpaperscissors/assets/${computerplay()}--left.png`;
+})
+
+
+
 function computerplay() {
     let answer = Math.trunc(Math.random() * 3) + 1;
-    if (answer === 1) return "Rock";
-    else if (answer === 2) return "Paper";
-    else if (answer === 3) return "Scissor";
+    if (answer === 1) return "rock";
+    else if (answer === 2) return "paper";
+    else if (answer === 3) return "scissors";
 }
 
 function upperCase(str) {
     const lowerCase = str.toLowerCase();
+    // return lowerCase;
     const result = lowerCase[0].toUpperCase() + str.slice(1);
     return result;
 }
@@ -45,9 +84,9 @@ function game() {
     let score;
 
     for (let i = 0; i < 5; i++) {
-        playerInput = prompt(`Rock - Paper - Scissor :`);
+        playerInput = (prompt(`Rock - Paper - Scissor :`));
         const playerSelection = upperCase(playerInput);
-        const computerSelection = computerplay();
+        const computerSelection = upperCase(computerplay());
         // playRound(playerSelection, computerSelection);
         console.log(`Round ${i + 1}`);
         console.log(`your choice: ${playerSelection}`);
@@ -72,4 +111,4 @@ function game() {
 }
 
 
-game();
+// game();
